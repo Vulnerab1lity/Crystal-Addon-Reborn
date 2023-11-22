e165e685db969fa1ffdf73e7768a282f33d53c48
F_x <admin@mcfreedom.eu>
2023-11-22 17:14:07 +0100

    compat

--- src/main/java/com/crystaldevs/crystal/mixin/meteor/HttpMixin.java
+++ src/main/java/com/crystaldevs/crystal/mixin/meteor/HttpMixin.java
@@ -5,15 +5,15 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(Http.class)
+@Mixin(value = Http.class, remap = false, priority = 1001)
 public class HttpMixin {
-    @ModifyArg(method = "get", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/utils/network/Http$Request;<init>(Lmeteordevelopment/meteorclient/utils/network/Http$Method;Ljava/lang/String;)V"), remap = false)
+    @ModifyArg(method = "get", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/utils/network/Http$Request;<init>(Lmeteordevelopment/meteorclient/utils/network/Http$Method;Ljava/lang/String;)V"))
     private static String onGet(String url) {
         if (url.startsWith("https://meteorclient.com/api")) return "http://0.0.0.0";
         return url;
     }
 
-    @ModifyArg(method = "post", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/utils/network/Http$Request;<init>(Lmeteordevelopment/meteorclient/utils/network/Http$Method;Ljava/lang/String;)V"), remap = false)
+    @ModifyArg(method = "post", at = @At(value = "INVOKE", target = "Lmeteordevelopment/meteorclient/utils/network/Http$Request;<init>(Lmeteordevelopment/meteorclient/utils/network/Http$Method;Ljava/lang/String;)V"))
     private static String onPost(String url) {
         if (url.startsWith("https://meteorclient.com/api")) return "http://0.0.0.0";
         return url;
